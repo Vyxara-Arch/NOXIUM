27ac904ed5735e6735aa057e2991e45271ce4bd9
MintyExtremum <139152392+mintyextremum@users.noreply.github.com>
2026-01-08 19:16:39 +0500

    Reformat OmegaTools class and strip_metadata method

--- core/omega.py
+++ core/omega.py
@@ -5,15 +5,16 @@ from PIL import Image
 class OmegaTools:
     @staticmethod
     def strip_metadata(image_path: str):
-        """Removes GPS and Camera data from images."""
+
         try:
             img = Image.open(image_path)
             data = list(img.getdata())
             img_no_exif = Image.new(img.mode, img.size)
             img_no_exif.putdata(data)
 
-            # Overwrite original
+
             img_no_exif.save(image_path)
             return True, "Metadata stripped cleanly."
         except Exception as e:
             return False, str(e)
+
