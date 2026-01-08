0518948883b95f28b9fe1fca76b604490b20c807
MintyExtremum <139152392+mintyextremum@users.noreply.github.com>
2026-01-08 19:15:46 +0500

    Reformat Shredder class and wipe_file method

--- core/shredder.py
+++ core/shredder.py
@@ -14,22 +14,23 @@ class Shredder:
         file_size = os.path.getsize(file_path)
 
         with open(file_path, "wb") as f:
-            # Pass 1: All Zeros
+
             f.write(b"\x00" * file_size)
             f.flush()
             os.fsync(f.fileno())
             f.seek(0)
 
-            # Pass 2: All Ones
+
             f.write(b"\xff" * file_size)
             f.flush()
             os.fsync(f.fileno())
             f.seek(0)
 
-            # Pass 3: Random Data
+
             f.write(secrets.token_bytes(file_size))
             f.flush()
             os.fsync(f.fileno())
 
-        # Final Step: Delete file
+
         os.remove(file_path)
+
